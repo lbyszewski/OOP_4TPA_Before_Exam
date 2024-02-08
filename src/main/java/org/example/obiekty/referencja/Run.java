35851b9d41b7dd601a1f146447f2b6b9d1a9bdee
inz Lukasz Byszewski <gdx153483@wsb.gda.pl>
2024-02-08 19:24:54 +0100

    JAVA

--- src/main/java/org/example/obiekty/referencja/Run.java
+++ src/main/java/org/example/obiekty/referencja/Run.java
@@ -1,6 +1,7 @@
 package org.example.obiekty.referencja;
 
 import org.example.Hermetyzacja.EncapsMethod;
+import org.example.Hermetyzacja.Encapsulation;
 import org.example.konstruktory.pola.FieldsAndConstructors;
 
 import java.util.Scanner;
@@ -11,6 +12,12 @@ public class Run {
         FieldsAndConstructors fieldsAndConstructors = new FieldsAndConstructors("Value 1", "Value 2");
         fieldsAndConstructors.someValueOne = "Something new 1";
         fieldsAndConstructors.someValueTwo = "Something new 2";
+
+        Encapsulation encapsulation = new Encapsulation("Same ABC","Same DEF");
+        encapsulation.setFirstValue("ABC");
+        encapsulation.setFirstValue("DEF");
+        encapsulation.getFirstValue();
+        encapsulation.getSecondValue();
 
         Scanner scanner = new Scanner(System.in);
 
