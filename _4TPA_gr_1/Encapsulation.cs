9c9cbfc566627431682ec109ce294d52959f82b8
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2024-02-13 10:30:06 +0100

    C#

---
+++ _4TPA_gr_1/Encapsulation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4TPA_gr_1
+{
+    public class Encapsulation
+    {
+        private string firstName;
+        private string lastName;
+
+        public Encapsulation(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+
+
+    }
+}
---
+++ _4TPA_gr_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4TPA_gr_1
+{
+    public  class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Encapsulation encapsulation = new Encapsulation("dfdsfdsdf", "fdfdfdfdf");
+
+
+            Console.WriteLine(encapsulation.FirstName + " " + encapsulation.LastName);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
